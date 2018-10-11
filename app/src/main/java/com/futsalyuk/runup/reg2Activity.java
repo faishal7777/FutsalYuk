eebc37bcca5a170d2f194fc0562c5a70cbd5352b
faishal <faishalrusydan7@gmail.com>
2018-10-11 21:46:33 +0700

    update acivity

---
+++ app/src/main/java/com/futsalyuk/runup/reg2Activity.java
@@ -0,0 +1,13 @@
+package com.futsalyuk.runup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class reg2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg2);
+    }
+}
