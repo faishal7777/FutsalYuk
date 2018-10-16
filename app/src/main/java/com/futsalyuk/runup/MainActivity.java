33e1ef246522fa703693cb6fc592727bab5f4b3f
Arkhi MS <arkhi07@hotmail.co.id>
2018-10-16 22:26:24 +0700

    tambah branch ben ga crash

--- app/src/main/java/com/futsalyuk/runup/MainActivity.java
+++ app/src/main/java/com/futsalyuk/runup/MainActivity.java
@@ -9,8 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.futsalyuk.runup.futsalyuk.R;
+//resolver R mati how
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/futsalyuk/runup/login_activity.java
+++ app/src/main/java/com/futsalyuk/runup/login_activity.java
@@ -3,8 +3,8 @@ package com.futsalyuk.runup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-
 import com.futsalyuk.runup.futsalyuk.R;
+// import android.R;
 
 public class login_activity extends AppCompatActivity {
 
