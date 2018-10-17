e0c480751c214142e0deb8db372adf8b6d241812
Arkhi MS <arkhi07@hotmail.co.id>
2018-10-17 19:32:41 +0700

    class login in progress editing

--- app/src/main/java/com/futsalyuk/runup/login_activity.java
+++ app/src/main/java/com/futsalyuk/runup/login_activity.java
@@ -3,9 +3,12 @@ package com.futsalyuk.runup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.content.Context;
+import android.widget.Toast;
+import android.widget.EditText;
 
 
-import com.futsalyuk.runup.futsalyuk.R;
+import com.futsalyuk.runup.futsalyuk.R; //iki R ga iso kampang 
 
 public class login_activity extends AppCompatActivity {
 
