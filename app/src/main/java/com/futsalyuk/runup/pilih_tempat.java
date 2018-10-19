a01141a3fbd5bea62cdc1b0fc255310e4cd609c9
faishal <faishalrusydan7@gmail.com>
2018-10-19 15:20:58 +0700

    update checkout 2.0

---
+++ app/src/main/java/com/futsalyuk/runup/pilih_tempat.java
@@ -0,0 +1,13 @@
+package com.futsalyuk.runup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pilih_tempat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_tempat);
+    }
+}
