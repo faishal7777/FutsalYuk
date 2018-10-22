bc1bac77499653a033776ebc89b6fe7478deb4bd
Alfarady <radenalfa9@gmail.com>
2018-10-22 18:44:32 +0700

    Alfa MatchMaking

--- app/src/main/java/com/futsalyuk/runup/temu_temanActivity.java
+++ app/src/main/java/com/futsalyuk/runup/temu_temanActivity.java
@@ -1,5 +1,7 @@
 package com.futsalyuk.runup;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,19 @@ public class temu_temanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temu_teman);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Apa anda serius akan meninggalkan game?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        embo.super.onBackPressed();
+                    }
+                }).create().show();
     }
 }
