5f9d0b785989b9f26360841fac025520eb6f3c4e
Alfarady <radenalfa9@gmail.com>
2018-10-22 14:37:07 +0700

    Alfa MatchMaking

--- app/src/main/java/com/futsalyuk/runup/ProfilFragment.java
+++ app/src/main/java/com/futsalyuk/runup/ProfilFragment.java
@@ -1,5 +1,6 @@
 package com.futsalyuk.runup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,12 +9,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.futsalyuk.runup.Models.CRUD_User;
 import com.futsalyuk.runup.futsalyuk.R;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class ProfilFragment extends Fragment {
+public class ProfilFragment extends Fragment implements View.OnClickListener {
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,8 @@ public class ProfilFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // -- inflate the layout for this fragment
         final View myInflatedView = inflater.inflate(R.layout.fragment_profil, container,false);
+        Button btnLogout = myInflatedView.findViewById(R.id.logoutBtn);
+        btnLogout.setOnClickListener(this);
 
         // Set the Text to try this out
         TextView t_Name = myInflatedView.findViewById(R.id.namaUser);
@@ -34,5 +41,12 @@ public class ProfilFragment extends Fragment {
         t_Pos.setText(CRUD_User.getPosition());
 
         return myInflatedView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        FirebaseAuth.getInstance().signOut();
+        Intent I = new Intent(new Intent(v.getContext(), login_activity.class));
+        startActivity(I);
     }
 }
